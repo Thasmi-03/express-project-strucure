743da4cfb9cd961eb94909c61f89f5ac95c6953b
Thasmiga <vimalthasmiga1112@gmail.com>
2025-09-29 14:14:32 +0530

    basic  server setup created

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 // Basic server setup
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 const PORT = 3000;
 
 // HTTP method handling
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
 });
 
-app.route('/book')
-    .get((req, res) => {
-        res.send('Get a random book')
-    })
-    .post((req, res) => {
-        res.send('Add a book')
-    })
-    .put((req, res) => {
-        res.send('Update the book')
-    })
+app
+  .route("/book")
+  .get((req, res) => {
+    res.send("Get a random book");
+  })
+  .post((req, res) => {
+    res.send("Add a book");
+  })
+  .put((req, res) => {
+    res.send("Update the book");
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
-
-
-
-
-
-
